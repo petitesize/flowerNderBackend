7da27fea8d0758112119b1c8aeec407fed558f58
Chanjoo Park <cjangel0503@gmail.com>
2024-02-22 20:10:08 +0900

    feat: orderDAO 주문 조회, 주문 수정 메서드 추가

--- src/data-access/orderDAO.js
+++ src/data-access/orderDAO.js
@@ -3,10 +3,10 @@ const { Order } = require("./model");
 const utils = require("../misc/utils");
 
 class OrderDAO {
-  async create({ orderItems, customerInfo, shippingInfo, deliveryFee }) {
+   async create({ orderItems, customerInfo, shippingInfo, deliveryFee }) {
    // 일단 모든 주문 착불(배송비 0)이지만 추후 배송비 입력 원할경우를 위해 변수 만들어 놓음
    deliveryFee = 0; 
-   
+
    // 배열로 받은 orderItems의 각 가격*수량+배송비 계산해서 더하기
    const orderAmount = orderItems.reduce((sum, item) => {
       sum += item.quantity*item.total_amount
@@ -27,7 +27,34 @@ class OrderDAO {
    });
    await order.save();
 
-   return OrderDAO.toObject();
+   return order.toObject();
+  }
+  
+  async findOrder(id, name, email) {
+   const order = await Order.findOne({
+      _id: id,
+      'customer_info.name': name,
+      'customer_info.email': email,
+   });
+   if (!order) {
+      throw new Error('주문을 찾을 수 없음');
+   }
+   return order.toObject();
+  }
+
+  async updateShippingInfo(id, shippingInfo) {
+   const orderUpdated = await Order.findByIdAndUpdate(id, {
+      'shipping_info.recipient': shippingInfo.recipient,
+      'customer_info.phone_number': shippingInfo.phone_number,
+      'shipping_info.address': shippingInfo.address,
+      'shipping_info.address_detail': shippingInfo.address_detail,
+   }, {
+      new: true
+   });
+   if (!orderUpdated) {
+      throw new Error('주문을 수정할 수 없음');
+   }
+   return orderUpdated.toObject();
   }
 }
 
---
+++ src/service/orderService.js
@@ -0,0 +1,9 @@
+const { orderDAO } = require("../data-access");
+const AppError = require("../misc/AppError");
+const commonErrors = require("../misc/commonErrors");
+
+class orderService {
+
+}
+
+module.exports = new orderService();
