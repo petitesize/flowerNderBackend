dd472f105795eb4e8eadfee7a64b1801fc60d558
parkjiwonnn <jiwonp05@gmail.com>
2024-02-23 21:36:36 +0900

    feat: 회원가입 완성

--- src/router/orderRouter.js
+++ src/router/orderRouter.js
@@ -11,14 +11,14 @@ orderRouter.post("/", orderController.postOrder);
 // req.params 객체를 통해 참조한 db document의 id는 동적 경로 매개변수
 orderRouter.get(
    "/:id", 
-   productMiddleware.checkOrderIdFrom("query"), 
+   orderMiddleware.checkOrderIdFrom("query"), 
    orderController.getOrder
 );
 
 // 배송지 수정 PATCH /api/v1/login
 orderRouter.patch(
    "/:id", 
-   productMiddleware.checkOrderIdFrom("query"), 
+   orderMiddleware.checkOrderIdFrom("query"), 
    orderController.patchOrder
 );
 
