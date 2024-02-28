1eabc213ecc8df46d20f5edd2983dd7843c6eb07
Chanjoo Park <cjangel0503@gmail.com>
2024-02-28 16:02:55 +0900

    [BE-24]feat: AWS S3관련 세팅

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -1,18 +1,8 @@
 const { productService } = require("../service");
 const utils = require("../misc/utils");
+const imageService = require('../service/imageService');
 
 const productController = {
-   // HTTP Post를 위한 controller(request handler)
-   async postProduct(req, res, next) {
-      try {
-         const { category, title, price, stock, description, size, origin, attribute, main_image, sub_image } = req.body;
-         const product = await productService.createProduct({ category, title, price, stock, description, size, origin, attribute, main_image, sub_image });
-         res.status(201).json(utils.buildResponse(product));
-      } catch (error) {
-         next(error);
-      }
-   },
-
    // HTTP GET를 위한 controller(request handler)
    // 상품 하나
    async getProduct(req, res, next) {
@@ -59,6 +49,36 @@ const productController = {
          res.json(utils.buildResponse(product));
       } catch (error) {
          next(error);
+      }
+   },
+
+   // HTTP Post를 위한 controller(request handler)
+   async postProduct(req, res, next) {
+      try {
+         const { category, title, price, stock, description, size, origin, attribute, main_image, sub_image } = req.body;
+         const product = await productService.createProduct({ category, title, price, stock, description, size, origin, attribute, main_image, sub_image });
+         res.status(201).json(utils.buildResponse(product));
+      } catch (error) {
+         next(error);
+      }
+   },
+
+   // 이미지 업로드 -> postProduct 함수랑 묶어야할 필요성 보임
+   async upload (req, res) {
+      try {
+        if (!req.file) {
+          return res.status(400).json({ error: '이미지 파일이 없습니다.' });
+        }
+    
+        const imageUrl = await imageService.uploadImageToS3(req.file);
+    
+        // 데이터베이스에 상품 정보와 imageUrl 저장하는 로직
+        // ...
+    
+        res.status(201).json({ imageUrl });
+      } catch (error) {
+        console.error('이미지 업로드 실패:', error);
+        res.status(500).json({ error: '이미지 업로드에 실패했습니다.' });
       }
    },
 };
