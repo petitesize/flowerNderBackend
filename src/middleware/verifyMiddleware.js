1a9de33124530c07b348d3199e828427ae545605
petitesize <petitesize22@gmail.com>
2024-06-05 16:55:04 +0900

    Feat: aws s3 버킷 설정 변경

--- src/middleware/verifyMiddleware.js
+++ src/middleware/verifyMiddleware.js
@@ -1,35 +1,31 @@
 const AppError = require("../misc/AppError");
 const commonErrors = require("../misc/commonErrors");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 function verifyToken(req, res, next) {
-   const token = req.headers['authorization'];
+  const token = req.headers["authorization"];
 
-   if (!token) {
-     next(
-      new AppError(
-        commonErrors.inputError,
-        `토큰이 제공되지 않았습니다.`,
-        403,
-      ),
+  if (!token) {
+    next(
+      new AppError(commonErrors.inputError, `토큰이 제공되지 않았습니다.`, 403)
     );
-   }
- 
-   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) {
-         next(
-            new AppError(
-               commonErrors.inputError,
-               `토큰이 제공되지 않았습니다.`,
-               403,
-            ),
-         )
-      }
-     req.user = decoded;
-     next();
-   });
- }
- 
- module.exports = {
-   verifyToken,
+  }
+  const tokenWithoutBearer = token.split(" ")[1];
+  jwt.verify(tokenWithoutBearer, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      next(
+        new AppError(
+          commonErrors.inputError,
+          `토큰이 제공되지 않았습니다.`,
+          403
+        )
+      );
+    }
+    req.user = decoded;
+    next();
+  });
+}
+
+module.exports = {
+  verifyToken,
 };
